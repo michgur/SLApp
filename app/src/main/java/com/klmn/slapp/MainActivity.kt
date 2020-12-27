c8da29fd40777e3d8cab76bda8ec75205dda8374
MichGur <michael@thegur.net>
2020-12-27 12:09:33 +0200

    added navigation controller

--- app/src/main/java/com/klmn/slapp/MainActivity.kt
+++ app/src/main/java/com/klmn/slapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.klmn.slapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment_container_view)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() =
+        navController.navigateUp() || super.onSupportNavigateUp()
 }
