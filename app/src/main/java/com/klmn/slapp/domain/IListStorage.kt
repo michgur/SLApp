0dc0e69deca36d84513aa294dae0dc92b0f282fd
MichGur <michael@thegur.net>
2020-12-29 11:18:49 +0200

    updated IListStorage

--- app/src/main/java/com/klmn/slapp/domain/IListStorage.kt
+++ app/src/main/java/com/klmn/slapp/domain/IListStorage.kt
@@ -6,7 +6,10 @@ import com.klmn.slapp.SlappList
 
 interface IListStorage {
     fun getList() : Task<SlappList>
-    fun updateList() : Task<Unit>
+    fun updateList(list: SlappList) : Task<Unit>
+    fun deleteList() : Task<SlappList>
     fun getItem(position: Int) : Task<SlappItem>
+    fun addItem(item: SlappItem) : Task<Unit>
     fun updateItem(position: Int, item: SlappItem) : Task<Unit>
+    fun removeItem(position: Int) : Task<SlappItem>
 }
