2a6a34a7ed305b974c1c6586d741fc5e5706fb0b
MichGur <michael@thegur.net>
2021-01-25 12:32:53 +0200

    fixed updateToken

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -29,33 +29,45 @@ exports.updateToken = functions.https.onRequest(async (req, res) => {
 
 exports.sendMessage = functions.https.onRequest(async (req, res) => {
     const listId = req.query.listId;
+    const uid = req.query.uid;
     const data = req.query.data;
+//    const data = {
+//        title: req.query.title,
+//        message: req.query.message
+//    };
 
     const list = await admin.firestore().collection('lists').doc(listId).get();
+
     const users = list.data().users;
+    const index = users.indexOf(uid);
+    if (index > -1) users.splice(index, 1);
+
     const tokens = users.map(async (uid) => {
         const userDoc = await admin.firestore().collection('users').doc(uid).get();
         return userDoc.data().token;
     });
 
-    const message = {
-        data: data,
-        tokens: tokens
-    };
-    admin.messaging().sendMulticast(message)
-        .then((response) => {
-            console.log(response.successCount + ' messages were sent successfully');
-            if (response.failureCount > 0) {
-                const failedTokens = [];
-                response.responses.forEach((resp, i) => {
-                    if (!resp.success) failedTokens.push(tokens[i]);
-                });
-                console.log('list of tokens that caused failures: ' + failedTokens);
-            }
-            res.json({ success: true });
-        })
-        .catch((error) => {
-            console.log('error sending message', error);
-            res.json({ success: false });
-        });
+    Promise.all(tokens).then((tokens) => {
+        const message = {
+            data: data,
+            tokens: tokens.filter(t => typeof t === 'string')
+        };
+        console.log(message.tokens);
+        admin.messaging().sendMulticast(message)
+            .then((response) => {
+                console.log(response.successCount + ' messages were sent successfully');
+                if (response.failureCount > 0) {
+                    const failedTokens = [];
+                    response.responses.forEach((resp, i) => {
+                        if (!resp.success) failedTokens.push(message.tokens[i]);
+                    });
+                    console.log('list of tokens that caused failures: ' + failedTokens);
+                }
+                res.json({ success: true });
+            })
+            .catch((error) => {
+                console.log('error sending message', error);
+                res.json({ success: false });
+            });
+    });
 });
